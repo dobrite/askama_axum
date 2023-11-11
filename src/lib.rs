398b21c50ee2e83c703727ca95d30dedd18ff6f1
Dave O <davidobrite@gmail.com>
2023-11-11 14:55:31 -0800

    chore(*): initial commit

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+#![forbid(unsafe_code)]
+#![deny(elided_lifetimes_in_paths)]
+#![deny(unreachable_pub)]
+
+pub use askama::*;
+pub use axum::response::{IntoResponse, Response};
+use http::StatusCode;
+
+pub fn into_response<T: Template>(t: &T) -> Response {
+    match t.render() {
+        Ok(body) => {
+            let headers = [(
+                http::header::CONTENT_TYPE,
+                http::HeaderValue::from_static(T::MIME_TYPE),
+            )];
+
+            (headers, body).into_response()
+        }
+        Err(_) => StatusCode::INTERNAL_SERVER_ERROR.into_response(),
+    }
+}
